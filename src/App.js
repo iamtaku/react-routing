0c2937473e145673b3709e85170a4e1f3a9d7dc4
Takuhei Mizutani <takuheimizutani@Takuheis-MacBook-Pro.local>
2020-12-10 20:14:22 +0900

    added new react route for show page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from "./Nav";
 import About from "./About";
 import Shop from "./Shop";
 import React from "react";
+import ItemDetail from "./ItemDetail";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path="/about" component={About} />
           <Route path="/shop" component={Shop} />
+          <Route path="/shop/:id" component={ItemDetail} />
         </Switch>
       </div>
     </Router>
--- src/Nav.js
+++ src/Nav.js
@@ -6,8 +6,12 @@ function Nav() {
     <nav>
       <h3>Logo</h3>
       <ul className="navlinks">
-        <li>About</li>
-        <li>Shop</li>
+        <Link to="/about">
+          <li>About</li>
+        </Link>
+        <Link to="/shop">
+          <li>Shop</li>
+        </Link>
       </ul>
     </nav>
   );
